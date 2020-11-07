ecaf5b1024dbae75be906711eca29dd21b1dd8d2
Arina-prog <58101558+Arina-prog@users.noreply.github.com>
2020-11-07 21:39:57 +0400

    Add files via upload

---
+++ task_1_7.py
@@ -0,0 +1,6 @@
+# Write a square root calculator that performs for some inputed value
+#.qarakusi armat hasvelu hashvich
+number = int(input())
+# degree = number**0.5
+degree = pow(number, 0.5)
+print(degree)
