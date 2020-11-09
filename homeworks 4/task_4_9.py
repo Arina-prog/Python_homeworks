33c34e680209044377a91f70a468ca55aaf88022
Arina-prog <arina.saiyan91@gmail.com>
2020-11-09 21:16:49 +0400

    tasks lesson 4_9

--- homeworks 4/task_4_9.py
+++ homeworks 4/task_4_9.py
@@ -2,3 +2,17 @@
 # 2 gold values, provide user 3 attempts to find all gold values
 #9... Определите игровое поле (матрица 2x2) с 2 значениями бомбы
 # и 2 значениями золота, предоставьте пользователю 3 попытки найти все значения золота
+count = 0
+game = [["bomb", "gold"], ["gold", "bomb"]]
+while(count < 3):
+    user = int(input("input  1, 2, 3 or 4:\n"))
+    if user == 1:
+        print(game[0][0])
+    if user == 2:
+        print(game[0][1])
+    if user == 3:
+        print(game[1][0])
+    if user == 4:
+        print(game[1][1])
+    count += 1
+
